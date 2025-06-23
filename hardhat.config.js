c3fbdbe1fd82c55a4df162afcce8add9c8977cf3
Tiago Tavares <tiago.tavares@parity.io>
2025-06-23 17:31:38 +0100

    feat: compile to evm

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,10 +2,39 @@ require("@parity/hardhat-polkadot");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+    },
+  },
+  // resolc: {
+  //   compilerSource: "npm",
+  //   settings: {
+  //     optimizer: {
+  //       enabled: true,
+  //       fallbackOz: true,
+  //     },
+  //   },
+  // },
+  // resolc: {
+  //   compilerSource: "binary",
+  //   settings: {
+  //     compilerPath:
+  //       "/Users/tiago/Projects/test-hardhat-polkadot/resolc-universal-apple-darwin",
+  //     optimizer: {
+  //       enabled: true,
+  //       fallbackOz: true,
+  //     },
+  //   },
+  // },
   networks: {
     hardhat: {
-      polkavm: true,
+      polkavm: false,
     },
   },
 };
